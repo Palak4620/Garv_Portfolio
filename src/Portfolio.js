1125cbf78971fecac23efbca34db5de0bef28239
Palak4620 <palakjaiswal00050@gmail.com>
2025-07-27 02:06:46 +0530

    added new vedio

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -92,7 +92,6 @@ const Portfolio = () => {
             I’m a video editor and motion designer with 5+ years of experience, specializing in social media, branding, and promotional content. I create clean, engaging visuals with sharp pacing and design-driven motion graphics to turn ideas into impactful results.          </p>
           <br></br>
           <br></br>
-          <br></br>
           {/* Location and Status */}
           <div className="flex flex-col gap-2 text-gray-400 mt-4 text-sm md:text-base">
             <span className="flex items-center gap-2">
