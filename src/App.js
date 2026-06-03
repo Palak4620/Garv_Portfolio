19c242467082851fd81e360c5258d2456519a5b4
Palak4620 <palakjaiswal00050@gmail.com>
2026-06-03 23:25:22 +0530

    Fix ESLint build errors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import React from 'react';
 import Portfolio from './Portfolio';
-import AboutSection from './AboutSection';
 import FooterSection from './FooterSection';
 import SkillsSection from './SkillsSection';
 import WorkSection from "./WorkSection"; 
-import VideoGallery from "./VideoGallery"; 
 
 function App() {
   return (
