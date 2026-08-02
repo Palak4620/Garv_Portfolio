e5c0db157dcf652dde679cd6f16067edc98480ad
Palak4620 <palakjaiswal00050@gmail.com>
2026-08-02 20:59:21 +0530

    added promo video

--- src/WorkSection.js
+++ src/WorkSection.js
@@ -28,20 +28,20 @@ const videoMap = {
     { title: "Promo Video", url: "https://youtu.be/9qKOWWM_bzM" },
     {
       title: "Promo Video",
+      url: "https://youtu.be/KUzLSydysVA?si=t41DFyDyecyJhgIR",
+    },
+    {
+      title: "Promo Video",
+      url: "https://youtu.be/n3ROR7e4iUo?si=3-lD-n6q3_MrxJLs",
+    },
+    {
+      title: "Promo Video",
       url: "https://youtu.be/gA13t1-zdVM?si=vrNmYhctbLYva4Pe",
     },
     {
       title: "Promo Video",
       url: "https://youtu.be/h2wzcmeoHuA?si=--qCOdepj3_2TKZn",
     },
-    {
-      title: "Promo Video",
-      url: "https://youtu.be/KUzLSydysVA?si=t41DFyDyecyJhgIR",
-    },
-    {
-      title: "Promo Video",
-      url: "https://youtu.be/n3ROR7e4iUo?si=3-lD-n6q3_MrxJLs",
-    }
   ],
   2: [
     { title: "Logo Title Animation 1", url: "https://youtu.be/x5cykILO1Rw" },
